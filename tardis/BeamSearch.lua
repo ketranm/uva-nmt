43df4b4e062dafe71f4a17a7a29cda01f6a7da0d
Ke Tran <m.k.tran@uva.nl>
2016-09-19 17:40:34 +0200

    working beamsearch

--- tardis/BeamSearch.lua
+++ tardis/BeamSearch.lua
@@ -5,14 +5,14 @@ local utils = require 'misc.utils'
 function BeamSearch:__init(opt)
     self.vocab = opt.vocab
     self.K = opt.beamSize or 10
-    self.Nw = opt.Nw or self.K
+    self.Nw = opt.Nw or self.K * self.K
     self.reverseInput = opt.reverseInput or true
 
     self.bosidx = self.vocab[2].word2idx['<s>']
     self.eosidx = self.vocab[2].word2idx['</s>']
     self.unkidx = self.vocab[2].word2idx['<unk>']
     self.padidx = self.vocab[2].word2idx['<pad>']
-    
+
     self._ignore = {[self.bosidx] = true, [self.eosidx] = true}
     self.normLength = true --opt.normLength
 end
@@ -54,86 +54,83 @@ function BeamSearch:run(x, maxLength)
     Beam search:
     - x: source sentence
     - maxLength: maximum length of the translation
-    - ref: opition, if it is provided, report sentence BLEU score
     ]]
     self.model:clearState()
+    --self.model:resetStates()
+    self.model.decoder.rememberStates = true
     local K, Nw = self.K, self.Nw
     local x = encodeString(x, self.vocab[1].word2idx, self.reverseInput)
+    -- not that if we do this, the first prediction will be the same
+    x = x:repeatTensor(K, 1)
     local srcLength = x:size(2)
     local T = maxLength or utils.round(srcLength * 1.4)
     self.model:stepEncoder(x)
 
     local hypos = torch.CudaTensor(K, T):fill(self.bosidx)
-    local curIdx = torch.CudaTensor(1, 1):fill(self.bosidx)
-    local logProb = self.model:stepDecoder(curIdx)
-    local maxScores, indices = logProb:topk(K, true)
-    local scores = maxScores:view(-1, 1)
-
-    hypos[{{}, 1}] = indices[1]
-    self.model:repeatState(K)
-
     local nbestCands = {}
     local nbestScores = {}
 
+    local scores = torch.CudaTensor(K, 1):zero()
     for t = 1, T-1 do
         local curIdx = hypos[{{}, {t}}]
         local logProb = self.model:stepDecoder(curIdx)
-        local maxScores, indices = logProb:topk(Nw, true)
-        local curScores = scores:repeatTensor(1, Nw)
-        maxScores:add(curScores)
+        -- quick hack to handle the first prediction
+        if t == 1 then
+            logProb[{{2, K}, {}}]:fill(-math.huge)
+        end
+        local maxscores, indices = logProb:topk(Nw, true)
+        local curscores = scores:repeatTensor(1, Nw)
+        maxscores:add(curscores)
 
-        local _scores, flatIdx = maxScores:view(-1):topk(K, true)
+        local _scores, flatIdx = maxscores:view(-1):topk(K, true)
         scores = _scores:view(-1, 1)
-        local nr, nc = maxScores:size(1), maxScores:size(2)
-
+        local nr, nc = maxscores:size(1), maxscores:size(2)
+        -- TODO: check with CudaLongTensor
         local rowIdx = flatIdx:long():add(-1):div(nc):add(1):cuda()
         local colIdx = indices:view(-1):index(1, flatIdx)
 
-        local xc = colIdx:eq(self.eosidx) --  completed candidates
+        local xc = colIdx:eq(self.eosidx) -- completed sentence
         local nx = xc:sum()
         if nx > 0 then
-            -- fond a candiate
+            -- found some candidates
             local cands = rowIdx:maskedSelect(xc)
-            local completedHypos = hypos:index(1, cands):narrow(2, 1, t)
+            local completedHyps = hypos:index(1, cands):narrow(2, 1, t)
             local xscores = scores:index(1, xc):view(-1)
-            for i = 1, nx do
-                local output = decodeString(completedHypos[i], self.vocab[2].idx2word, self._ignore)
-                local score = xscores[i]
-                if self.normLength then
-                    score = score / t
-                end
-                table.insert(nbestCands, output)
-                table.insert(nbestScores, score)
-            end
-            print(nbestCands)
-            if nx == colIdx:numel() then break end
 
-            local rc = colIdx:ne(self.eosidx) -- remain hypotheses to expand
-            rowIdx = rowIdx:maskedSelect(rc)
-            colIdx = colIdx:maskedSelect(rc)
-            scores = scores:maskedSelect(rc)
-            K = rc:sum()
-            if K == 0 then break end
+            -- add to nbest
+            for i = 1, nx do
+                local text = decodeString(completedHyps[i], self.vocab[2].idx2word, self._ignore)
+                local s = xscores[i] / t -- normalized
+                table.insert(nbestCands, text)
+                table.insert(nbestScores, s)
+            end
+
+            if nx == colIdx:numel() then break end
+            -- check remaining in-completed hypotheses
+            local rh = colIdx:ne(self.eosidx)
+            rowIdx = rowIdx:maskedSelect(rh)
+            colIdx = colIdx:maskedSelect(rh)
+            scores = scores:maskedSelect(rh)
+            -- reduce K
+            K = rh:sum()
         end
 
+        -- keep survival hypotheses
         hypos = hypos:index(1, rowIdx)
         hypos[{{}, {t+1}}] = colIdx
-        self.model:indexDecoderState(rowIdx)
+        self.model:indexStates(rowIdx)
     end
 
     if #nbestCands == 0 then
-        -- worst case scenario, we have to take the best possible candiate
         assert(K == self.K)
         scores = scores:view(-1)
         for i = 1, K do
-            local output = decodeString(hypos[i], self.vocab[2].idx2word, self._ignore)
-            local score = scores[i]
-            table.insert(nbestCands, output)
-            table.insert(nbestScores, score)
+            local text = decodeString(hypos[i], self.vocab[2].idx2word, self._ignore)
+            local s = scores[i] / T
+            table.insert(nbestCands, text)
+            table.insert(nbestScores, s)
         end
     end
-
-    -- pick the best translation candidates
-    local score, idx = torch.Tensor(nbestScores):topk(1, true)
+    local _, idx = torch.Tensor(nbestScores):topk(1, true)
     return nbestCands[idx[1]]
 end
