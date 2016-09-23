6a1883238669a8294d1d6d271a838918ecbbe879
Ke Tran <m.k.tran@uva.nl>
2016-09-23 10:35:53 +0200

    character NMT examples: 23.8 BLEU on TED, de->en

---
+++ charmain.lua
@@ -0,0 +1,126 @@
+-- This is NMT, you have to use CUDA
+-- the code is written for GPUs
+require 'nn'
+require 'cutorch'
+require 'cunn'
+
+require 'data.loadCharBitex'
+require 'tardis.CharNMT'
+require 'tardis.BeamSearch'
+
+
+local timer = torch.Timer()
+torch.manualSeed(42)
+local cfg = require 'pl.config'
+local opt = cfg.read(arg[1])
+
+if not opt.gpuid then opt.gpuid = 0 end
+torch.manualSeed(opt.seed or 42)
+cutorch.setDevice(opt.gpuid + 1)
+cutorch.manualSeed(opt.seed or 42)
+
+print('Experiment Setting: ', opt)
+io:flush()
+
+
+local loader = DataLoader(opt)
+opt.padIdx = loader.padIdx
+
+opt.word2char = loader.word2char
+opt.featureMaps = {50, 100, 150, 200, 200, 200, 200}
+opt.kernels = {1, 2, 3, 4, 5, 6, 7}
+opt.charSize = 15
+local model = nn.NMT(opt)
+
+-- prepare data
+function prepro(input)
+    local x, y = unpack(input)
+    local seqlen = y:size(2)
+    -- make contiguous and transfer to gpu
+    x = x:contiguous():cuda()
+    prev_y = y:narrow(2, 1, seqlen-1):contiguous():cuda()
+    next_y = y:narrow(2, 2, seqlen-1):contiguous():cuda()
+
+    return x, prev_y, next_y
+end
+
+function train()
+    local exp = math.exp
+    local nupdates = 0
+    for epoch = 1, opt.maxEpoch do
+        loader:train()
+        model:training()
+        local nll = 0
+        local nbatches = loader.nbatches
+        local totwords = 0
+        timer:reset()
+        print('learningRate: ', opt.learningRate)
+        for i = 1, nbatches do
+            local x, prev_y, next_y = prepro(loader:next())
+
+            nll = nll + model:forward({x, prev_y}, next_y)
+            model:backward({x, prev_y}, next_y)
+            model:update(opt.learningRate)
+            --nll = nll + model:optimize({x, prev_y}, next_y)
+            model:clearState()
+            nupdates = nupdates + 1
+            totwords = totwords + prev_y:numel()
+            if i % opt.reportEvery == 0 then
+                local floatEpoch = (i / nbatches) + epoch - 1
+                local msg = 'epoch %.4f / %d   [ppl] %.4f   [speed] %.2f w/s   [update] %.3f K'
+                local args = {msg, floatEpoch, opt.maxEpoch, exp(nll/i), totwords / timer:time().real, nupdates/1000}
+                print(string.format(unpack(args)))
+                collectgarbage()
+            end
+        end
+        if epoch >= opt.decayAfter then
+            opt.learningRate = opt.learningRate * 0.5
+        end
+
+        loader:valid()
+        model:evaluate()
+        local nll = 0 -- validation loss
+        local nbatches = loader.nbatches
+        for i = 1, nbatches do
+            local x, prev_y, next_y = prepro(loader:next())
+            nll = nll + model:forward({x, prev_y}, next_y:view(-1))
+            if i % 50 == 0 then collectgarbage() end
+        end
+
+        local modelFile = string.format("%s/tardis_%d_%.4f.t7", opt.modelDir, epoch, nll/nbatches)
+        paths.mkdir(paths.dirname(modelFile))
+        model:save(modelFile)
+
+        local msg = '\nvalidation\nEpoch %d valid ppl %.4f\nsaved model %s'
+        local args = {msg, epoch, exp(nll/nbatches), modelFile}
+        print(string.format(unpack(args)))
+    end
+end
+
+local eval = opt.modelFile and opt.textFile
+
+if not eval then
+    train()
+else
+    opt.transFile =  opt.transFile or 'translation.txt'
+
+    local startTime = timer:time().real
+    print(string.format('Loading model: %s', opt.modelFile))
+    model:load(opt.modelFile)
+    local file = io.open(opt.transFile, 'w')
+    -- create beam search object
+    opt.vocab = loader.vocab
+    local bs = BeamSearch(opt)
+    bs:use(model)
+    local refLine
+    local nlines = 0
+    for line in io.lines(opt.textFile) do
+        local translation = bs:run(line, opt.maxLength)
+        nlines = nlines + 1
+        file:write(translation .. '\n')
+        file:flush()
+        io.write(string.format('translated sentence %d\r', nlines))
+        io.flush()
+    end
+    file:close()
+end
---
+++ tardis/CharNMT.lua
@@ -0,0 +1,208 @@
+-- author: Ke Tran <m.k.tran@uva.nl>
+
+require 'tardis.GlimpseDot'
+require 'optim'
+require 'tardis.FastTransducer'
+require 'tardis.CharTransducer'
+local model_utils = require 'tardis.model_utils'
+
+local utils = require 'misc.utils'
+
+local NMT, parent = torch.class('nn.NMT', 'nn.Module')
+
+function NMT:__init(opt)
+    -- build encoder
+    local sourceSize = opt.sourceSize
+    local inputSize = opt.embeddingSize
+    local hiddenSize = opt.hiddenSize
+    self.encoder = nn.CharTransducer(opt)
+
+    -- build decoder
+    local targetSize = opt.targetSize
+    self.decoder = nn.Transducer(targetSize, inputSize, hiddenSize, opt.numLayers, opt.dropout)
+
+    -- attention
+    self.glimpse = nn.GlimpseDot()
+
+    self.layer = nn.Sequential()
+    self.layer:add(nn.JoinTable(3))
+    self.layer:add(nn.View(-1, 2 * hiddenSize))
+    self.layer:add(nn.Linear(2 * hiddenSize, hiddenSize, false))
+    self.layer:add(nn.Tanh())
+    self.layer:add(nn.Linear(hiddenSize, targetSize, true))
+    self.layer:add(nn.LogSoftMax())
+
+    local weights = torch.ones(targetSize)
+    weights[opt.padIdx] = 0
+
+    self.sizeAverage = true
+    self.padIdx = opt.padIdx
+    self.criterion = nn.ClassNLLCriterion(weights, false)
+    self.tot = torch.CudaTensor() -- count non padding symbols
+    self.numSamples = 0
+
+    -- convert to cuda
+    self.encoder:cuda()
+    self.decoder:cuda()
+    self.glimpse:cuda()
+    self.layer:cuda()
+    self.criterion:cuda()
+
+    self.params, self.gradParams =
+        model_utils.combine_all_parameters(self.encoder,
+                                           self.decoder,
+                                           self.glimpse,
+                                           self.layer)
+    self.maxNorm = opt.maxNorm or 5
+    -- for optim
+    self.optimConfig = {}
+    self.optimStates = {}
+
+    --self:reset()
+end
+
+function NMT:reset()
+    self.params:uniform(-0.1, 0.1)
+end
+
+function NMT:forward(input, target)
+    --[[ Forward pass of NMT
+
+    Parameters:
+    - `input` : table of source and target tensor
+    - `target` : a tensor of next words
+
+    Return:
+    - `logProb` : negative log-likelihood of the mini-batch
+    --]]
+    local target = target:view(-1)
+    self:stepEncoder(input[1])
+
+    local logProb = self:stepDecoder(input[2])
+    self.tot:resizeAs(target)
+    self.tot:ne(target, self.padIdx)
+    self.numSamples = self.tot:sum()
+    local nll = self.criterion:forward(logProb, target)
+    return nll / self.numSamples
+end
+
+function NMT:backward(input, target)
+    -- zero grad manually here
+    self.gradParams:zero()
+    local gradXent = self.criterion:backward(self.logProb, target:view(-1))
+    local scale = 1 / (self.sizeAverage and self.numSamples or 1)
+    gradXent:mul(scale)
+
+    local gradLayer = self.layer:backward({self.cntx, self.decOutput}, gradXent)
+
+    local gradDecoder = gradLayer[2] -- grad to decoder
+    local gradGlimpse =
+        self.glimpse:backward({self.encOutput, self.decOutput}, gradLayer[1])
+
+    gradDecoder:add(gradGlimpse[2]) -- accumulate gradient in-place
+
+    self.decoder:backward(input[2], gradDecoder)
+
+    -- initialize gradient from decoder
+    local gradStates = self.decoder:gradStates()
+    self.encoder:setGradStates(gradStates)
+    -- backward to encoder
+    local gradEncoder = gradGlimpse[1]
+    self.encoder:backward(input[1], gradEncoder)
+end
+
+function NMT:update(learningRate)
+    local gradNorm = self.gradParams:norm()
+    local scale = learningRate
+    if gradNorm > self.maxNorm then
+        scale = scale * self.maxNorm / gradNorm
+    end
+    self.params:add(self.gradParams:mul(-scale)) -- do it in-place
+end
+
+function NMT:optimize(input, target)
+    local feval = function(x)
+        if self.params ~= x then
+            self.params:copy(x)
+        end
+        local f = self:forward(input, target)
+        self:backward(input, target)
+        local gradNorm = self.gradParams:norm()
+        -- clip gradient
+        if gradNorm > self.maxNorm then
+            self.gradParams:mul(self.maxNorm / gradNorm)
+        end
+        return f, self.gradParams
+    end
+    local _, fx = optim.adam(feval, self.params, self.optimConfig, self.optimStates)
+    return fx[1]
+end
+
+function NMT:parameters()
+    return self.params
+end
+
+function NMT:training()
+    self.encoder:training()
+    self.decoder:training()
+end
+
+function NMT:evaluate()
+    self.encoder:evaluate()
+    self.decoder:evaluate()
+end
+
+function NMT:load(fileName)
+    local params = torch.load(fileName)
+    self.params:copy(params)
+end
+
+function NMT:save(fileName)
+    torch.save(fileName, self.params)
+end
+
+-- useful interface for beam search
+function NMT:stepEncoder(x)
+    --[[ Encode the source sequence
+    Parameters:
+    - `x` : source tensor, can be a matrix (batch)
+    --]]
+    self.encOutput = self.encoder:forward(x)
+    self.prevStates = self.encoder:lastStates()
+    return self.encOutput
+end
+
+function NMT:stepDecoder(x)
+    --[[ Run the decoder
+    If it is called for the first time, the decoder will be initialized
+    from the last state of the encoder. Otherwise, it will continue from
+    its last state. This is useful for beam search or reinforce training
+
+    Parameters:
+    - `x` : target sequence, can be a matrix (batch)
+
+    Return:
+    - `logProb` : cross entropy loss of the sequence
+    --]]
+    self.decoder:setStates(self.prevStates)
+    self.decOutput = self.decoder:forward(x)
+    self.cntx = self.glimpse:forward{self.encOutput, self.decOutput}
+    self.logProb = self.layer:forward{self.cntx, self.decOutput}
+
+    -- update prevStates
+    self.prevStates = self.decoder:lastStates()
+    return self.logProb
+end
+
+function NMT:indexStates(index)
+    self.encOutput = self.encOutput:index(1, index)
+    self.decoder:indexStates(index)
+    self.prevStates = self.decoder:lastStates()
+end
+
+function NMT:clearState()
+    self.encoder:clearState()
+    self.decoder:clearState()
+    self.layer:clearState()
+    self.glimpse:clearState()
+end
