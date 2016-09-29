d75848d66c8a45e3fec318464027342057985a6e
Ke Tran <ketranmanh@gmail.com>
2016-09-29 22:35:18 +0200

    simple rnn language model

---
+++ examples/LM.lua
@@ -0,0 +1,93 @@
+-- author: Ke Tran <m.k.tran@uva.nl>
+require 'optim'
+require 'cudnn'
+local LM, parent = torch.class('nn.LM', 'nn.Module')
+
+function LM:__init(opt)
+    local V = opt.vocabSize
+    local D = opt.inputSize
+    local H = opt.hiddenSize
+    local padidx = opt.padIdx
+    self.net = nn.Sequential()
+    self.net:add(nn.LookupTable(V, D, padidx, 1, 2))
+    self.net:add(cudnn.GRU(D, H, opt.numLayers, true, opt.dropout))
+    self.net:add(nn.View(-1, H))
+    self.net:add(nn.Linear(H, V))
+    self.net:add(nn.LogSoftMax())
+
+    local weights = torch.ones(V)
+    weights[opt.padIdx] = 0
+    self.net:cuda()
+    self.params, self.gradParams = self.net:getParameters()
+
+    self.maxNorm = opt.maxNorm or 5
+    -- for optim
+    self.optimConfig = {}
+    self.optimStates = {}
+    self:reset(4e-2)
+end
+
+function LM:reset(std)
+    self.params:uniform(-std, std)
+end
+
+function LM:forward(input, target)
+    local target = target:view(-1)
+    self.logp = self.net:forward(input)
+    return self.criterion:forward(self.logp, target)
+end
+
+function LM:backward(input, target)
+    -- zero grad manually here
+    local dw = self.criterion:backward(self.logp, target:view(-1))
+    self.net:backward(input, dw)
+end
+
+function LM:update(learningRate)
+    local gradNorm = self.gradParams:norm()
+    local scale = learningRate
+    if gradNorm > self.maxNorm then
+        scale = scale * self.maxNorm / gradNorm
+    end
+    self.params:add(self.gradParams:mul(-scale)) -- do it in-place
+end
+
+function LM:optimize(input, target)
+    local feval = function(x)
+        if self.params ~= x then
+            self.params:copy(x)
+        end
+        self.gradParams:zero()
+        local f = self:forward(input, target)
+        self:backward(input, target)
+        local gradNorm = self.gradParams:norm()
+        -- clip gradient
+        if gradNorm > self.maxNorm then
+            self.gradParams:mul(self.maxNorm / gradNorm)
+        end
+        return f, self.gradParams
+    end
+    local _, fx = optim.adam(feval, self.params, self.optimConfig, self.optimStates)
+    return fx[1]
+end
+
+function LM:parameters()
+    return self.params
+end
+
+function LM:training()
+    self.net:training()
+end
+
+function LM:evaluate()
+    self.net:evaluate()
+end
+
+function LM:load(fileName)
+    local params = torch.load(fileName)
+    self.params:copy(params)
+end
+
+function LM:save(fileName)
+    torch.save(fileName, self.params)
+end
---
+++ trainlm.lua
@@ -0,0 +1,93 @@
+-- This is NMT, you have to use CUDA
+-- the code is written for GPUs
+require 'nn'
+require 'cutorch'
+require 'cunn'
+require 'data.loadText'
+require 'examples.LM'
+
+local cmd = torch.CmdLine()
+cmd:text('GRU Language Model')
+cmd:option('-datapath', '../data', 'location of data')
+cmd:option('-modelDir', '../model', 'location of data')
+cmd:option('-batchSize', 128, 'size of minibatch')
+cmd:option('-inputSize', 1024, 'embedding size')
+cmd:option('-hiddenSize', 1024, 'GRU size')
+cmd:option('-numLayers', 3, 'number of layers in stacked GRU')
+cmd:option('-maxNorm', 5, 'max gradient norm')
+cmd:option('-gpuid', 0, 'GPU device')
+cmd:text()
+opt = cmd:parse(arg or {})
+
+local timer = torch.Timer()
+torch.manualSeed(42)
+if not opt.gpuid then opt.gpuid = 0 end
+torch.manualSeed(opt.seed or 42)
+cutorch.setDevice(opt.gpuid + 1)
+cutorch.manualSeed(opt.seed or 42)
+
+print('Experiment Setting: ', opt)
+io:flush()
+local loader = DataLoader(opt)
+opt.padIdx = 1
+opt.vocabSize = loader.vocabSize
+local model = nn.LM(opt)
+
+-- prepare data
+function prepro(x)
+    local T = x:size(2)
+    -- make contiguous and transfer to gpu
+    local input = x:narrow(2, 1, T-1):contiguous():cuda()
+    local target = x:narrow(2, 2, T-1):contiguous():cuda()
+    return input, target
+end
+
+function train()
+    local exp = math.exp
+    local nupdates = 0
+    for epoch = 1, opt.maxEpoch do
+        loader:train()
+        model:training()
+        local nll = 0
+        local nbatches = loader.nbatches
+        local totwords = 0
+        timer:reset()
+        print('learningRate: ', opt.learningRate)
+        for i = 1, nbatches do
+            local x, y = prepro(loader:next())
+            nll = nll + model:optimize(x, y)
+            nupdates = nupdates + 1
+            totwords = totwords + prev_y:numel()
+            if i % opt.reportEvery == 0 then
+                local floatEpoch = (i / nbatches) + epoch - 1
+                local msg = 'epoch %.4f / %d   [ppl] %.4f   [speed] %.2f w/s [update] %.3f'
+                local args = {msg, floatEpoch, opt.maxEpoch, exp(nll/i), totwords / timer:time().real, nupdates/1000}
+                print(string.format(unpack(args)))
+                collectgarbage()
+            end
+        end
+        if epoch >= opt.decayAfter then
+            opt.learningRate = opt.learningRate * 0.5
+        end
+
+        loader:valid()
+        model:evaluate()
+        local nll = 0 -- validation loss
+        local nbatches = loader.nbatches
+        for i = 1, nbatches do
+            local x, y = prepro(loader:next())
+            nll = nll + model:forward(x, y)
+            if i % 200 == 0 then collectgarbage() end
+        end
+
+        local modelFile = string.format("%s/tardis_%d_%.4f.t7", opt.modelDir, epoch, nll/nbatches)
+        paths.mkdir(paths.dirname(modelFile))
+        model:save(modelFile)
+
+        local msg = '\nvalidation\nEpoch %d valid ppl %.4f\nsaved model %s'
+        local args = {msg, epoch, exp(nll/nbatches), modelFile}
+        print(string.format(unpack(args)))
+    end
+end
+
+train()
