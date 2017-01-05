669e16ce6a5a111cd8012b2fb497833277a7bc5d
Ekaterina Garmash <ekaterinagarmash@vpn-staff-146-50-68-192.vpn.uva.nl>
2017-01-05 15:16:43 +0100

    modified data/loadBitext.lua to process held out ensemble bitext

--- data/loadBitext.lua
+++ data/loadBitext.lua
@@ -21,6 +21,14 @@ function DataLoader:__init(opt)
     -- auxiliary file to store additional information about shards
     local indexfile = path.join(opt.dataPath, 'index.t7')
     self.vocab = {}
+    if opt.useTrainVocab then -- in case we're working with the heldout files for ensemble analysis
+        self.vocab = torch.load(vocabfile)
+        if not path.exists(indexfile) then
+            print('=> create ensemble heldout tensor files...')
+            self:text2tensor(trainfiles, opt.shardSize, opt.batchSize, self.tracker[1])
+            torch.save(indexfile, self.tracker)
+        end
+    end
     if not path.exists(vocabfile) then
         print('=> creating source vocabulary ...')
         self:shortlist(opt.sourceSize)
