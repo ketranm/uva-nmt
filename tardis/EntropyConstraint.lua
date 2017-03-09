e5065766b1d50c9548ad350ffe6543d612d6ac35
Ekaterina Garmash <ekaterinagarmash@vpn-staff-146-50-68-107.vpn.uva.nl>
2017-03-10 00:52:39 +0100

    topK entropy constraint

--- tardis/EntropyConstraint.lua
+++ tardis/EntropyConstraint.lua
@@ -8,22 +8,52 @@ function EntropyConstraint:__init(opt)
 		self.normalize = 1
 	end
 
+	if opt.approxEntropyK ~= nil  then
+		self.approxEntropyK = opt.approxEntropyK
+	else
+		self.approxEntropyK = 0
+	end
+
 end
 
+function getTopKDistribution(output,topK)
+	
+	local vals,_ = output:topk(topK,true)
+	return vals
+end
 
-function EntropyConstraint:forward(inputLogProb)
+function getTopKDistributionNonZero(output,topK)
+	local _,indices = output:topk(topK,true)
+	local result = torch.zeros(output:size())
+	for i=1,topK do
+		result[indices[i]] = 1
+		output[indices]
+	end
+	return result
+end
+
+function EntropyConstraint:forward(logProb)
+
+	local inputLogProb = logProb:view(-1)
 	local probDistr = torch.exp(inputLogProb)
+	if self.approxEntropyK > 0 then
+		probDistr = torch.cmul(probDistr,getTopKDistributionNonZero(inputLogProb,self.approxEntropyK))
+	end
+	
 	local negEntropy = torch.sum(torch.cmul(inputLogProb,probDistr))
 	self.entropy = torch.mul(negEntropy,-1*self.normalize)
 	local averagedEntropy = torch.sum(self.entropy)/self.entropy:size(1)
 	return averagedEntropy
-end
+endh
 
 function EntropyConstraint:backward(inputLogProb)
 	local probDistr = torch.exp(inputLogProb)
+	if self.approxEntropyK > 0 then
+		probDistr = torch.cmul(probDistr,getTopKDistributionNonZero(inputLogProb,self.approxEntropyK))
+	end
 	local normalizedLogProb = torch.mul(inputLogProb,self.normalize)
 	normalizedLogProb:csub(self.entropy:expandAs(normalizedLogProb))
-	local gradEntropy = torch.sum(torch.cmul(probDistr,normalizedLogProb))
+	local gradEntropy = torch.cmul(probDistr,normalizedLogProb)
 	return gradEntropy
 end
 
