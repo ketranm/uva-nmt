eb5d3cef0bc5a9f0243e27cd577f6497ad061491
Ekaterina Garmash <egarmas1@smtgpu1.science.uva.nl>
2017-03-10 00:55:38 +0100

    .

--- tardis/EntropyConstraint.lua
+++ tardis/EntropyConstraint.lua
@@ -3,7 +3,7 @@ local EntropyConstraint,parent = torch.class('nn.EntropyConstraint', 'nn.Criteri
 
 function EntropyConstraint:__init(opt)
 	if opt.normalizeEntropy == 1 then
-		self.normalize = 1/torch.log(self.targetSize)
+		self.normalize = 1/torch.log(opt.targetSize)
 	else
 		self.normalize = 1
 	end
@@ -13,17 +13,17 @@ end
 
 function EntropyConstraint:forward(inputLogProb)
 	local probDistr = torch.exp(inputLogProb)
-	local negEntropy = torch.sum(torch.cmul(inputLogProb,probDistr))
-	self.entropy = torch.mul(negEntropy,-1*self.normalize)
-	local averagedEntropy = torch.sum(self.entropy)/self.entropy:size(1)
+	local negEntropy = torch.sum(torch.cmul(inputLogProb,probDistr),2)
+	self.negEntropy = torch.mul(negEntropy,self.normalize)
+	local averagedEntropy = torch.sum(self.negEntropy)/self.negEntropy:size(1)
 	return averagedEntropy
 end
 
 function EntropyConstraint:backward(inputLogProb)
 	local probDistr = torch.exp(inputLogProb)
 	local normalizedLogProb = torch.mul(inputLogProb,self.normalize)
-	normalizedLogProb:csub(self.entropy:expandAs(normalizedLogProb))
-	local gradEntropy = torch.sum(torch.cmul(probDistr,normalizedLogProb))
+	torch.add(normalizedLogProb,self.negEntropy:expandAs(normalizedLogProb))
+	local gradEntropy = torch.cmul(probDistr,normalizedLogProb)
 	return gradEntropy
 end
 
